105c8f6046f4a812c61b39408be9cb9b5b14226f
Thorsten Schlathölter <thorsten.schlathoelter@postfinance.ch>
2021-11-11 16:41:41 +0100

    Adds criteria queries for Message and ScenarioExecution

--- simulator-starter/src/main/java/org/citrusframework/simulator/repository/MessageRepositoryCustom.java
+++ simulator-starter/src/main/java/org/citrusframework/simulator/repository/MessageRepositoryCustom.java
@@ -1,14 +1,10 @@
 package org.citrusframework.simulator.repository;
 
-import java.util.Collection;
-import java.util.Date;
+import org.citrusframework.simulator.model.Message;
+import org.citrusframework.simulator.model.MessageFilter;
+
 import java.util.List;
 
-import org.citrusframework.simulator.model.Message;
-import org.springframework.data.domain.Pageable;
-
 public interface MessageRepositoryCustom {
-    List<Message> findByDateBetweenAndDirectionInAndPayloadContainingIgnoreCase(Date fromDate,
-                                                                                Date toDate, Collection<Message.Direction> directions, String containingText,
-                                                                                String headerParameterName, String headerParameterValue, Pageable pageable);
+    List<Message> find(MessageFilter filter);
 }
