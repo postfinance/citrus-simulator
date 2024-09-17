8b73c6783202502de77dcf46fcb10c0369e55182
muellerfluri <flurina.mueller.2@postfinance>
2024-09-17 12:16:41 +0200

    fix(citrus-simulator-ui): run lint fix

--- simulator-ui/tests/error-banner.spec.ts
+++ simulator-ui/tests/error-banner.spec.ts
@@ -4,11 +4,11 @@ import {
   mockErrorResponseForAllNavbarLinkedSites
 } from './helpers/helper-functions';
 
-test.beforeEach(async ({ page }) => {
+test.beforeEach(async ({page}) => {
   await page.goto('http://localhost:9000/');
 });
 
-test('should show error banner if there is an error code in the backend response while loading any page', async ({ page }) => {
+test('should show error banner if there is an error code in the backend response while loading any page', async ({page}) => {
   await mockErrorResponseForAllNavbarLinkedSites(page);
 
   await goToAllNavigationTabsAndOptionallyValidateContent(page, verifyErrorBannerIsVisible);
--- simulator-ui/tests/scenario.spec.ts
+++ simulator-ui/tests/scenario.spec.ts
@@ -1,31 +1,31 @@
-import { expect, Page, test } from '@playwright/test';
-import { mockBackendResponse } from './helpers/helper-functions';
+import {expect, Page, test} from '@playwright/test';
+import {mockBackendResponse} from './helpers/helper-functions';
 
 const scenarioJson = [
-  { name: 'Default', type: 'MESSAGE_TRIGGERED' },
-  { name: 'Fail', type: 'MESSAGE_TRIGGERED' },
-  { name: 'GoodBye', type: 'MESSAGE_TRIGGERED' },
-  { name: 'GoodNight', type: 'MESSAeGE_TRIGGERED' },
-  { name: 'Hello', type: 'MESSAGE_TRIGGERED' },
-  { name: 'Howdy', type: 'MESSAGE_TRIGGERED' },
-  { name: 'Parm', type: 'MESSAGE_TRIGGERED' },
-  { name: 'Throw', type: 'MESSAGE_TRIGGERED' },
-  { name: 'ByeStarter', type: 'STARTER' },
-  { name: 'HiStarter', type: 'STARTER' },
+  {name: 'Default', type: 'MESSAGE_TRIGGERED'},
+  {name: 'Fail', type: 'MESSAGE_TRIGGERED'},
+  {name: 'GoodBye', type: 'MESSAGE_TRIGGERED'},
+  {name: 'GoodNight', type: 'MESSAeGE_TRIGGERED'},
+  {name: 'Hello', type: 'MESSAGE_TRIGGERED'},
+  {name: 'Howdy', type: 'MESSAGE_TRIGGERED'},
+  {name: 'Parm', type: 'MESSAGE_TRIGGERED'},
+  {name: 'Throw', type: 'MESSAGE_TRIGGERED'},
+  {name: 'ByeStarter', type: 'STARTER'},
+  {name: 'HiStarter', type: 'STARTER'},
 ];
 
 const addititonalPageJson = [
-  { name: 'One', type: 'STARTER' },
-  { name: 'Two', type: 'STARTER' },
-  { name: 'Three', type: 'STARTER' },
+  {name: 'One', type: 'STARTER'},
+  {name: 'Two', type: 'STARTER'},
+  {name: 'Three', type: 'STARTER'},
 ];
 
 const orderedJson = [
-  { name: 'Fail', type: 'MESSAGE_TRIGGERED' },
-  { name: 'Parm', type: 'MESSAGE_TRIGGERED' },
-  { name: 'Default', type: 'MESSAGE_TRIGGERED' },
-  { name: 'HiStarter', type: 'STARTER' },
-  { name: 'ByeStarter', type: 'STARTER' },
+  {name: 'Fail', type: 'MESSAGE_TRIGGERED'},
+  {name: 'Parm', type: 'MESSAGE_TRIGGERED'},
+  {name: 'Default', type: 'MESSAGE_TRIGGERED'},
+  {name: 'HiStarter', type: 'STARTER'},
+  {name: 'ByeStarter', type: 'STARTER'},
 ];
 
 const parameterJson = [
@@ -42,14 +42,14 @@ const parameterJson = [
 
 const availableScenarios: number = addititonalPageJson.length + scenarioJson.length;
 
-test.beforeEach(async ({ page }) => {
-  await mockBackendResponse(page, '**/api/scenarios**page=0**', scenarioJson, { 'x-total-count': availableScenarios.toString() });
+test.beforeEach(async ({page}) => {
+  await mockBackendResponse(page, '**/api/scenarios**page=0**', scenarioJson, {'x-total-count': availableScenarios.toString()});
   await page.goto('http://localhost:9000/scenario');
   await page.getByTestId('itemsPerPageSelect').selectOption('10');
 });
 
-test('should display all scenario information of a starter scenario', async ({ page }) => {
-  await mockBackendResponse(page, '**/api/scenarios**', [{ name: 'Test', type: 'STARTER' }], { 'x-total-count': '1' });
+test('should display all scenario information of a starter scenario', async ({page}) => {
+  await mockBackendResponse(page, '**/api/scenarios**', [{name: 'Test', type: 'STARTER'}], {'x-total-count': '1'});
 
   await page.goto('http://localhost:9000/scenario');
 
@@ -60,7 +60,7 @@ test('should display all scenario information of a starter scenario', async ({ p
   await expect(page.getByTestId('scenarioExecutionButton')).toBeVisible();
 });
 
-test('should display all scenario information of a non-starter scenario', async ({ page }) => {
+test('should display all scenario information of a non-starter scenario', async ({page}) => {
   await mockBackendResponse(
     page,
     '**/api/scenarios**',
@@ -70,7 +70,7 @@ test('should display all scenario information of a non-starter scenario', async 
         type: 'MESSAGE_TRIGGERED',
       },
     ],
-    { 'x-total-count': '1' },
+    {'x-total-count': '1'},
   );
 
   await page.goto('http://localhost:9000/scenario');
@@ -81,21 +81,21 @@ test('should display all scenario information of a non-starter scenario', async 
   await expect(page.getByTestId('scenarioExecutionButton')).toBeVisible();
 });
 
-test('should have the first 10 of 13 elements displayed in the table and display right nb of visible elements', async ({ page }) => {
+test('should have the first 10 of 13 elements displayed in the table and display right nb of visible elements', async ({page}) => {
   await checkIfAllJsonContentIsVisible(page, 10, availableScenarios, scenarioJson);
 });
 
-test('should have the last 3 of 13 elements displayed in the table after clicking on 2. page', async ({ page }) => {
+test('should have the last 3 of 13 elements displayed in the table after clicking on 2. page', async ({page}) => {
   await mockBackendResponse(page, '**/api/scenarios**page=1**', addititonalPageJson);
-  await page.getByRole('link', { name: '2' }).click();
+  await page.getByRole('link', {name: '2'}).click();
   await checkIfAllJsonContentIsVisible(page, 10, availableScenarios, addititonalPageJson);
 });
 
-test('see if frontend trusts backend to send only as much data as requested', async ({ page }) => {
+test('see if frontend trusts backend to send only as much data as requested', async ({page}) => {
   const selectedPageSize = 10;
   await expect(page.getByTestId('itemsPerPageSelect')).toHaveValue('10');
 
-  scenarioJson.push({ name: 'One', type: 'STARTER' });
+  scenarioJson.push({name: 'One', type: 'STARTER'});
   await page.goto('http://localhost:9000/scenario');
 
   await page.getByTestId('itemsPerPageSelect').selectOption(selectedPageSize.toString());
@@ -105,31 +105,31 @@ test('see if frontend trusts backend to send only as much data as requested', as
   scenarioJson.pop();
 });
 
-test('text filter input should apply and clear filter button should reset it', async ({ page }) => {
+test('text filter input should apply and clear filter button should reset it', async ({page}) => {
   await applyFilterAAndCheckCorrectness(page);
   await page.getByTestId('clearFilterButton').click();
 
   await expect(page.getByTestId('scenarioFilterByNameInput')).toHaveValue('');
 });
 
-test('should have updated displayed scenarios after refresh button was clicked and a new scenario received', async ({ page }) => {
+test('should have updated displayed scenarios after refresh button was clicked and a new scenario received', async ({page}) => {
   const selectOptionsForNumberOfScenariosToDisplay: number[] = [10, 20, 50, 100];
   for (const option of selectOptionsForNumberOfScenariosToDisplay) {
     await page.getByTestId('itemsPerPageSelect').selectOption(option.toString());
     scenarioJson.pop();
-    await mockBackendResponse(page, '**/api/scenarios**', scenarioJson, { 'x-total-count': scenarioJson.length.toString() });
+    await mockBackendResponse(page, '**/api/scenarios**', scenarioJson, {'x-total-count': scenarioJson.length.toString()});
 
     await page.getByTestId('refreshListButton').click();
     await checkIfAllJsonContentIsVisible(page, option, scenarioJson.length, scenarioJson);
-    scenarioJson.push({ name: 'HiStarter', type: 'STARTER' });
+    scenarioJson.push({name: 'HiStarter', type: 'STARTER'});
   }
 });
 
-test('should move to the scenario-execution page with right filter entered if clicked on the execution button', async ({ page }) => {
+test('should move to the scenario-execution page with right filter entered if clicked on the execution button', async ({page}) => {
   const scenarioName = 'Test';
   const urlRegex = new RegExp(`.*scenario-result\\?.*filter%5BscenarioName\\.equals%5D=${scenarioName}`);
 
-  await mockBackendResponse(page, '**/api/scenarios**', [{ name: scenarioName, type: 'STARTER' }]);
+  await mockBackendResponse(page, '**/api/scenarios**', [{name: scenarioName, type: 'STARTER'}]);
   await page.goto('http://localhost:9000/scenario');
   await page.getByTestId('scenarioExecutionButton').click();
 
@@ -137,11 +137,11 @@ test('should move to the scenario-execution page with right filter entered if cl
   await expect(page.getByTestId('scenarioExecutionFilterInput')).toHaveValue(scenarioName);
 });
 
-test('should launch the scenario-execution and popup should be visible if clicked on the launch button', async ({ page }) => {
+test('should launch the scenario-execution and popup should be visible if clicked on the launch button', async ({page}) => {
   const scenarioId = [7];
   const scenarioName = 'Test';
   const urlRegex = new RegExp(`.*scenario-result\\?.*filter%5BexecutionId\\.in%5D=${scenarioId[0].toString()}`);
-  await mockBackendResponse(page, '**/api/scenarios**', [{ name: scenarioName, type: 'STARTER' }]);
+  await mockBackendResponse(page, '**/api/scenarios**', [{name: scenarioName, type: 'STARTER'}]);
 
   await page.goto('http://localhost:9000/scenario');
   await mockBackendResponse(page, `**/api/scenarios/${scenarioName}/launch**`, scenarioId);
@@ -154,9 +154,9 @@ test('should launch the scenario-execution and popup should be visible if clicke
   await expect(page.getByText('executionId.in: ' + scenarioId[0])).toBeVisible();
 });
 
-test('should show error message if launch failed after click on the launch button', async ({ page }) => {
+test('should show error message if launch failed after click on the launch button', async ({page}) => {
   const scenarioName = 'Test';
-  await mockBackendResponse(page, '**/api/scenarios**', [{ name: scenarioName, type: 'STARTER' }]);
+  await mockBackendResponse(page, '**/api/scenarios**', [{name: scenarioName, type: 'STARTER'}]);
 
   await page.goto('http://localhost:9000/scenario');
   await page.getByTestId('scenarioLaunchButton').click();
@@ -165,7 +165,7 @@ test('should show error message if launch failed after click on the launch butto
   await expect(page).toHaveURL(/.*localhost:9000\/scenario/);
 });
 
-test('should go to detail view of a scenario type MESSAGE_TRIGGERED check for content and and then go back', async ({ page }) => {
+test('should go to detail view of a scenario type MESSAGE_TRIGGERED check for content and and then go back', async ({page}) => {
   const allVisibleDetailElements = [
     'scenarioDetailsHeading',
     'scenarioDetailsName',
@@ -183,7 +183,7 @@ test('should go to detail view of a scenario type MESSAGE_TRIGGERED check for co
   await expect(page).toHaveURL(/.*scenario/);
 });
 
-test('should go to detail view of a scenario type STARTER check for content and go back', async ({ page }) => {
+test('should go to detail view of a scenario type STARTER check for content and go back', async ({page}) => {
   await mockBackendResponse(page, '**/api/scenarios/ByeStarter/parameters', parameterJson);
   const allVisibleDetailElements = [
     'scenarioDetailsHeading',
@@ -227,7 +227,7 @@ const applyFilterAAndCheckCorrectness = async (page: Page): Promise<any> => {
   const pageSize: number = 20;
   await page.getByTestId('itemsPerPageSelect').selectOption(pageSize.toString());
   await checkIfAllJsonContentIsVisible(page, pageSize, availableScenarios, scenarioJson);
-  await mockBackendResponse(page, '**/api/scenarios?**nameContains=a&sort=id,asc', orderedJson, { 'x-total-count': '5' });
+  await mockBackendResponse(page, '**/api/scenarios?**nameContains=a&sort=id,asc', orderedJson, {'x-total-count': '5'});
 
   await page.getByTestId('scenarioFilterByNameInput').fill('a');
 
